a438273587a9df2c6a5dcb4156dfe5904b929d92
Surondesh Daulagupu <rondaulagupu8@gmail.com>
2024-07-28 17:42:56 +0530

    added basic hilt setup

--- app/src/main/java/com/target/targetcasestudy/MainActivity.kt
+++ app/src/main/java/com/target/targetcasestudy/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
